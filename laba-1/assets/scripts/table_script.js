38b32eeb67700808e63c0741b7f639678fa2aed3
Маша Замотохина <zamotokhina.masha@mail.ru>
2023-10-30 21:45:29 +0400

    5 лаба

---
+++ laba-1/assets/scripts/table_script.js
@@ -0,0 +1,5 @@
+function add(){
+    document.getElementById("id-table").value = document.getElementById("name").textContent;
+    document.getElementById("id-table").value = document.getElementById("course").textContent;
+    document.getElementById("id-table").value = document.getElementById("gtihub").textContent;   
+}
